b9c388eb4956ed502a4617344f2eb10e72775911
SAGAR KUMAR <109574777+sagarsuthar385@users.noreply.github.com>
2022-08-08 19:29:12 +0530

    Add files via upload

---
+++ Institute/Main_file.py
@@ -0,0 +1,48 @@
+import Faculty,Student
+print("          ")
+print("--------------------------------------------------------------")
+print("----------------------Institute Detail------------------------")
+print("          ")
+status=True
+while status:
+    choice=input("Do you want to make detail press'f' for faculty and press 's' for student= ") 
+    if choice=="f":
+        name=input("Enter your name= ")
+        position=input("Enter your postion in this institute= ")
+        subject=input("Enter your teaching subject= ")
+        contact_no=int(input("Enter your contact number= "))
+        email=input("Enter your email address= ")
+        score=int(input("Enter the score= "))
+        city=input("Enter your city name= ")
+        print("            ")
+        print("------------------------------------------------------")   
+        f=Faculty.facultyclass() 
+        f.create_faculty(name,position,subject,contact_no,email,score,city)
+        print("------------------------------------------------------")   
+        print("            ")
+
+    else:
+        name=input("Enter your name= ")
+        Institude_name=input("Enter your institute name= ")
+        Class=int(input("Enter your class= "))
+        contact_no=int(input("Enter your contact number= "))
+        email=input("Enter your email address= ")
+        score=int(input("Enter the score= "))
+        city=input("Enter your city name= ")
+        print("            ")
+        print("------------------------------------------------------")    
+        f=Student.studentclass() 
+        f.create_student(name,Institude_name,Class,contact_no,email,score,city) 
+        f.setemail('asfdskfd')
+        print(f.email) 
+        print("------------------------------------------------------")   
+        print("            ")
+
+    choice=input("Do you want to make detail press'y' for yes and press 'n' for no= ")
+    if choice=='y':
+        status=True
+    else:
+        status=False
+            
+
+           
---
+++ Institute/Student.py
@@ -0,0 +1,30 @@
+class studentclass:
+    name=''
+    Institude_name=''
+    Class=''
+    contact_no=''
+    email=''
+    score=''
+    city=''
+ 
+    dict={}
+    def create_student(self,name,Institude_name,Class,contact_no,email,score,city):
+        innerdict={}
+        self.name=name
+        self.Institude_name=Institude_name
+        self.Class=Class
+        self.contact_no=contact_no
+        self.email=email
+        self.score=score
+        self.city=city
+
+        innerdict['Institude_name']=self.Institude_name
+        innerdict['Class']=self.Class
+        innerdict['contact_no']=self.contact_no
+        innerdict['email']=self.email
+        innerdict['score']=self.score
+        innerdict['city']=self.city
+        self.dict[name]=innerdict
+        print(self.dict)
+    def setemail(self,email):
+        self.email=email
